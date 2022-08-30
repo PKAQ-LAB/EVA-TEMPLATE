297638b6a6a232002010082bfdf0b5ef1fadcd82
PKAQ <pkaq@msn.com>
2022-08-30 22:09:42 +0800

    version update

--- api/src/main/java/io/nerv/WebBooter.java
+++ api/src/main/java/io/nerv/WebBooter.java
@@ -1,7 +1,5 @@
 package io.nerv;
 
-import io.nerv.core.license.LicenseVerify;
-import io.nerv.properties.EvaConfig;
 import io.nerv.web.sys.dict.cache.DictCacheHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,30 +20,13 @@ import org.springframework.context.annotation.ComponentScan;
 public class WebBooter implements CommandLineRunner {
 
     @Autowired
-    private EvaConfig evaConfig;
-
-    @Autowired(required = false)
-    private LicenseVerify licenseVerify;
-
-    @Autowired
     private DictCacheHelper dictCacheHelper;
 
     @Override
     public void run(String... args) {
         log.info(" ---- 字典初始化 开始 ---- ");
-        this.dictCacheHelper.init();
         this.dictCacheHelper.getAll();
         log.info(" ---- 字典初始化 结束 ---- ");
-        if (evaConfig.getLicense().isEnable()){
-            // 安装license
-            licenseVerify.init();
-
-            // 验证license
-            if (!licenseVerify.vertify()) {
-                log.error("授权验证未通过, 请更新授权文件");
-                Runtime.getRuntime().halt(1);
-            }
-        }
     }
 
     public static void main(String[] args) {
